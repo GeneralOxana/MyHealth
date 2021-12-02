f54b539d77282f1205a34c3be5040304fe045387
GeneralOxana <okcanochka@ya.ru>
2021-12-03 00:04:32 +0400

    Initial commit

---
+++ app/src/main/java/com/example/myhealth/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.myhealth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(q: Bundle?) {
+        super.onCreate(q)
+        setContentView(R.layout.activity_main)
+
+    }
+
+
+
+
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+}
